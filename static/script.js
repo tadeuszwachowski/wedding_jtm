d053cedf26014cd1502c03483d04e5b977cd61a3
tadeuszwachowski <37869895+tadeuszwachowski@users.noreply.github.com>
2023-09-06 03:49:57 +0200

    Add songs and score

--- static/script.js
+++ static/script.js
@@ -1,11 +1,11 @@
 window.onload = function () {
-    console.log('onload')
     addEventListener("keypress", (e) => {
         if (e.code == 'Space') switchButton();
     });
     
     // Counter
-    var seconds = 90;
+    // var seconds = 90;
+    document.getElementById("ddlViewBy").value = 0;
     document.getElementById("seconds").innerHTML = seconds;
     var tens = 0;
     var stop = false;
@@ -18,7 +18,8 @@ window.onload = function () {
     var player = document.getElementById('player');
     var sourceMp3 = document.getElementById('sourceMp3');
 
-    buttonStopwatch.onclick = switchButton();
+    // buttonStopwatch.onclick = switchButton();
+    buttonStopwatch.onclick = () => switchButton();
     
     function switchButton() {
         
@@ -56,8 +57,12 @@ window.onload = function () {
       if (seconds <= 0){
         appendSeconds.innerHTML = "00";
         clearInterval(Interval);
+        if (score > 0) {
+          window.location.href = 'bacca.html'
+        } else {
+          alert('Spróbuj zgadnąć choć jedną!')
+        }
       }
-    
     }
     
     // function loadSong(songNo) {
@@ -68,15 +73,22 @@ window.onload = function () {
     ans.style.display = "none";
     loadSong(1);
 }
-
+var seconds = 90; 
 var song = 1;
+var tens = 0;
+var guessed = 0;
+var stop = false;
 var player=document.getElementById('player');
 var sourceMp3=document.getElementById('sourceMp3');
+var songId = document.getElementById('song-id');
+var score = 0;
+var scoreboard = document.getElementById('score');
 
 function loadSong(songNo) {
     
     sourceMp3.src='songs/' + songNo + '.mp3';
     song = songNo;
+    songId.innerHTML = 'Piosenka ' + songNo;
     player.load();
     // player.play();
     console.log('song', songNo, 'loaded')
@@ -89,4 +101,28 @@ function playSong() {
 function stopSong() {
     player.pause();
     player.currentTime = 0;
+}
+
+function checkSong() {
+  var selectedSong = document.getElementById("ddlViewBy").value;
+  console.log(selectedSong);
+  if (selectedSong == song) {
+    scoreboard.innerHTML = parseInt((90-seconds)*100);
+    guessed++;
+    // song++;
+    // songId.innerHTML = 'Piosenka ' + song;
+  } else {
+    alert('Wtopa! Ale lecimy dalej');
+    // song++;
+    // songId.innerHTML = 'Piosenka ' + song;
+  }
+  song++;
+  songId.innerHTML = 'Piosenka ' + song;
+  loadSong(song); 
+
+  if (song >= 11) {
+    window.location.href = 'bacca.html';
+  }
+
+  document.getElementById("ddlViewBy").value = 0;
 }
